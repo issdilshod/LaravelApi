2a9fae905db4f52e19313d666247193dce6e7b2a
issdilshod <dilshod@themembers.com>
2022-04-21 20:27:54 +0400

    Task #7: Add validation

--- app/Http/Controllers/Api/DeskController.php
+++ app/Http/Controllers/Api/DeskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\DeskStoreRequest;
 use App\Http\Resources\DeskResource;
 use App\Models\Desk;
 use Illuminate\Http\Request;
@@ -25,9 +26,9 @@ class DeskController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(DeskStoreRequest $request)
     {
-        $created_desk = Desk::create($request->all());
+        $created_desk = Desk::create($request->validated());
 
         return new DeskResource($created_desk);
     }
